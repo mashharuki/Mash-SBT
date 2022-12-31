1fd270624c2616d22745cca2a1e478d48059c90f
Haruki Kondo <44923695+mashharuki@users.noreply.github.com>
2022-12-31 23:37:28 +0900

    修正

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,10 +3,13 @@ const { ethers } = require('hardhat');
 async function main() {
   const MashSBT = await ethers.getContractFactory('MashSBT');
   const mashSBT = await MashSBT.deploy();
-
+  // contract deploy
   await mashSBT.deployed();
 
   console.log(`CooperatorNFT deployed to ${mashSBT.address}`);
+
+  // SBT Mintec
+  
 }
 
 // We recommend this pattern to be able to use async/await everywhere
